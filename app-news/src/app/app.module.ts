1605e1bb1368d86fba185af2924791f209259db9
davidUrielMT <144058751+davidUrielMT@users.noreply.github.com>
2023-11-15 16:15:50 -0600

    Add files via upload

---
+++ app-news/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    //AngularFireModule,
+  ],
+   
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+
---
+++ app-news/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBbKrDmq8COFgpo3ivU7vMwox5pujyYZxI",
+    authDomain: "app-noticias-4e4b8.firebaseapp.com",
+    projectId: "app-noticias-4e4b8",
+    storageBucket: "app-noticias-4e4b8.appspot.com",
+    messagingSenderId: "683443719819",
+    appId: "1:683443719819:web:a533f71afd8fe3d6ec0e8e",
+    measurementId: "G-T8D8WF8Y2W"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
